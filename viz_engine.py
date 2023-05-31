c12478bc25f870bde75709f53125a0291e951a1b
Robert Mifsud <robert.mifs@gmail.com>
2023-05-31 10:40:29 +0200

    Plot support and resistance trends

--- viz_engine.py
+++ viz_engine.py
@@ -7,17 +7,43 @@ def plt_stock(df: pd.DataFrame):
     df_ohlc = df[['Open', 'High', 'Low', 'Close']]
 
     # Apply smoothing with simple moving average
-    legend = ['Moving Average (10)']
+    legend = [
+        'Resistance Trend',
+        'Support Trend',
+        'Moving Average (10)'
+    ]
+
     smooth = df['Close'].rolling(window=10).mean()
-    add_plots={
-        'Moving Average (10)' : mpf.make_addplot(smooth, color='orange')
-    }
+    add_plots=[
+        mpf.make_addplot(smooth, color='orange')
+    ]
 
-    fig, ax = mpf.plot(df_ohlc, type='candle', style='charles', title='AAPL', addplot=list(add_plots.values()), returnfig=True)
+    # Trend lines 2011-12-16 to 2013-12-13
+    dates=[
+        '2011-11-25',
+        '2012-04-10',
+        '2012-05-18',
+        '2012-09-21',
+        '2012-11-16',
+        '2012-12-03',
+        '2013-04-19',
+        '2013-05-07',
+        '2013-06-28',
+        '2013-12-19'
+    ]
 
-    ax[0].legend([None]*(len(add_plots)+2))
+    datepairs = [(dates[i], dates[i+1]) for i in range(0,9)]
+
+    tlines_list = [
+        dict(tlines=datepairs, tline_use='high', colors='r'),
+        dict(tlines=datepairs, tline_use='low', colors='g')
+    ]
+
+    fig, ax = mpf.plot(df_ohlc, type='candle', style='charles', title='AAPL', addplot=add_plots, returnfig=True, tlines=tlines_list)
+
+    ax[0].legend([None]*(3+2))
     handles = ax[0].get_legend().legendHandles
-    ax[0].legend(handles=handles[2:],labels=list(add_plots.keys()))
+    ax[0].legend(handles=handles[2:],labels=legend)
 
 dir_archive = os.path.join(os.curdir, 'archive')
 dir_test = os.path.join(dir_archive, 'AAPL_2006-01-01_to_2018-01-01.csv')
